41d0d35d9d6c5e68ac59562a3773351557c16648
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2024-01-10 04:21:00 +0000

    delete

--- python-more_data_structures/python-more_data_structures/1-search_replace.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    replaced = []
-    for x in my_list:
-        if x == search:
-            x = replace
-        replaced.append(x)
-    return replaced
