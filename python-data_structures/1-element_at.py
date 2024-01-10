121d6ac0d72439083f3231c62e0c95a4f57e53a0
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2024-01-09 21:38:10 -0600

    push complete file

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Write a function that retrieves an element from a list.
+# Prototype: def element_at(my_list, idx):
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
