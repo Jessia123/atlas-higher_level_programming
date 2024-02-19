a28dd1b2e3a06522983009b05109e6292f3a078d
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2024-02-19 06:05:19 +0000

    oops

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,18 +1,2 @@
-#!/bin/bash
-
--- MySQL root username and password
-MYSQL_USER="root"
-MYSQL_PASSWORD="your_mysql_root_password"
-
--- Database name to create
-DATABASE_NAME="hbtn_0c_0"
-
--- Attempt to create the database if it doesn't already exist
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
-
--- Check if the database was created or already existed
-if [ $? -eq 0 ]; then
-    echo "Database $DATABASE_NAME created successfully or already exists."
-else
-    echo "Failed to create database $DATABASE_NAME. Check your MySQL credentials."
-fi
+-- Write a script that creates the database hbtn_0c_0 in your MySQL server.
+CREATE database IF NOT EXISTS hbtn_0c_0;
