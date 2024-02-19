163d46a347fe757a784b69512f0ec555711da33e
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2024-02-19 05:58:16 +0000

    creating a database

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# MySQL root username and password
+MYSQL_USER="root"
+MYSQL_PASSWORD="your_mysql_root_password"
+
+# Database name to create
+DATABASE_NAME="hbtn_0c_0"
+
+# Check if the database already exists
+EXISTING_DATABASE=$(mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$DATABASE_NAME'" | grep -v '^SCHEMA_NAME$')
+
+# If the database does not exist, create it
+if [ -z "$EXISTING_DATABASE" ]; then
+    echo "Creating database $DATABASE_NAME"
+    mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE $DATABASE_NAME;"
+    echo "Database created successfully."
+else
+    echo "Database $DATABASE_NAME already exists."
+fi
